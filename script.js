77a27d01acb003d9c2d0e264d8866deec3006c61
Thierry <varelathierry@gmail.com>
2022-07-05 20:57:28 -0300

    ajustando a funcionalidade de aumentar os quadrados e criando o minimo de 5 e maximo de 50

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function aleatorio() {
   const rgb = `rgb(${aleatory}, ${aleatory2}, ${aleatory3})`;
   return rgb;
 }
-
 const cores = ['preto', aleatorio(), aleatorio(), aleatorio()];
 const body = document.body;
 const criarTitulo = document.createElement('h1');
@@ -27,7 +26,6 @@ function divColheCor(event) {
   selector.classList.remove('selected');
   event.target.classList.add('selected');
   const cor = event.target.style.backgroundColor;
-  // console.log(cor)
   return cor;
 }
 function criarPaleta() {
@@ -61,6 +59,38 @@ function apagar() {
     pixels[i].style.backgroundColor = 'white';
   }
 }
+function criarInput() {
+  const input = document.createElement('input');
+  input.setAttribute('id', 'board-size');
+  input.style.marginLeft = '30px'
+  body.appendChild(input);
+}
+criarInput();
+//window.alert("Board inválido!");
+function recebeValor() {
+  const tamanho = document.getElementById('board-size');
+  let numero = tamanho.value;
+  limpaBloco();
+  criarBloco(numero);
+  criarPixel(numero);
+}
+function criarButtonVQV() {
+  const criarBotao = document.createElement('button');
+  criarBotao.setAttribute('id', 'clear-board');
+  criarBotao.innerText = 'VQV';
+  criarBotao.style.border = 'black solid 1px';
+  criarBotao.style.width = '45px';
+  criarBotao.style.height = '30px';
+  criarBotao.style.marginLeft = '10px';
+  criarBotao.style.borderRadius = '15px';
+  criarBotao.style.backgroundColor = '#2A6B53';
+  criarBotao.style.color = 'white';
+  criarBotao.style.fontFamily = 'arial';
+  body.appendChild(criarBotao);
+  criarBotao.addEventListener('click', recebeValor);
+}
+ criarButtonVQV();
+
 function criarButton() {
   const criarBotao = document.createElement('button');
   criarBotao.setAttribute('id', 'clear-board');
@@ -68,7 +98,7 @@ function criarButton() {
   criarBotao.style.border = 'black solid 1px';
   criarBotao.style.width = '60px';
   criarBotao.style.height = '45px';
-  criarBotao.style.marginLeft = '300px';
+  criarBotao.style.marginLeft = '50px';
   criarBotao.style.borderRadius = '15px';
   criarBotao.style.backgroundColor = 'black';
   criarBotao.style.color = 'white';
@@ -77,11 +107,23 @@ function criarButton() {
   criarBotao.addEventListener('click', apagar);
 }
 criarButton();
-function criarBloco() {
+
+
+function criarBloco(valor) {
   const criarDivPixel = document.createElement('div');
+  if (valor==undefined || valor<5){
+    valor = 5
+    }
+    else if(valor>50)
+    valor = 50
   body.appendChild(criarDivPixel);
   criarDivPixel.style.marginTop = '20px';
-  criarDivPixel.style.width = '210px';
+  if (valor==undefined)
+    valor = 5
+  console.log(valor)
+  let largura = (valor * 42)
+  let string = `${largura}px`
+  criarDivPixel.style.width = string
   criarDivPixel.style.height = '210px';
   criarDivPixel.style.marginLeft = '20px';
   criarDivPixel.style.fontSize = '0px';
@@ -94,10 +136,18 @@ function recebeCor(event) {
   const cor = estilo.getPropertyValue('background-Color');
   event.target.style.backgroundColor = cor;
 }
-function criarPixel() {
+
+
+function criarPixel(valor) {
+  limpaPixel();
   const pixelBlock = document.getElementById('pixel-board');
-  for (let i = 0; i < 5; i += 1) {
-    for (let y = 0; y < 5; y += 1) {
+  if (valor==undefined || valor<5){
+  valor = 5
+  }
+  else if(valor>50)
+  valor = 50
+  for (let i = valor; i > 0; i -= 1) {
+    for (let y = valor; y > 0; y -= 1) {
       const criarDivFilho = document.createElement('div');
       criarDivFilho.className = 'pixel';
       pixelBlock.appendChild(criarDivFilho);
@@ -110,6 +160,17 @@ function criarPixel() {
     }
   }
 }
+function limpaPixel() {
+  let pixels = document.getElementsByClassName('pixel')
+  for(let i = pixels.length -1; i>=0; i-=1) {
+  pixels[i].remove(pixels[i])
+}
+}
+function limpaBloco() {
+  let bloco = document.getElementById('pixel-board');
+  bloco.remove()
+}
+
 
 criarBloco();
 criarPixel();
